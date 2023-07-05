4327fc9ae21ea3decb68062b57b314855d594b42
Angshu101 <36377710+Angshu101@users.noreply.github.com>
2023-07-05 21:06:09 +0530

    Vite project Created

---
+++ 100xdev/ReactFirstProject/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+var todo=[{
+titile:"Go to gym",
+description:"At 7 am ",
+}];
+
+function App() {
+  return (
+    <>
+      <h1>HI From React</h1>
+      </>
+  )
+}
+
+export default App
