df2eda53865c48a7abfd524f686ec146e84b8388
Angshu101 <36377710+Angshu101@users.noreply.github.com>
2023-07-09 09:24:52 +0530

    CourseSellingProject

--- 100xdev/ReactFirstProject/src/App.jsx
+++ 100xdev/ReactFirstProject/src/App.jsx
@@ -6,14 +6,43 @@ import './App.css'
 var todo=[{
 titile:"Go to gym",
 description:"At 7 am ",
+id:1
+},
+{
+titile:"Go to swiming",
+description:"At 9 am ",
+id:2
 }];
 
 function App() {
-  return (
-    <>
-      <h1>HI From React</h1>
-      </>
-  )
+  const [todo, setTodo] = useState({
+    title: "go to gym",
+    description: "go to mall",
+    id: 1
+  });
+  setInterval(() => {
+    setTodo({
+      title: "go eat something",
+      description: "eat food",
+      id: 3
+    });
+  }, 2000);
+
+return (
+  <div>
+    <h5>hi there</h5>
+    <span>{todo.title}</span>
+    {todo.description}
+    {todo.id}
+    <PersonName firstName={todo.title} lastName={"Majumdar"}></PersonName>
+  </div>
+)
+}
+
+function PersonName(props) {
+  return <div>
+   {props.firstName} {props.lastName}
+  </div>
 }
 
 export default App
---
+++ 100xdev/TODOReactAPP/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  return (
+    <div>
+    
+    </div>
+  )
+}
+function Todo_d(props){
+  <div>
+   
+  </div>
+}
+
+
+export default App
+
+
+// {mytodo.map((todo)=>{
+//   return <div>
+//     {todo.title}
+//     {todo.description}
+//     <br />
+//   </div>
+  
+// })}
